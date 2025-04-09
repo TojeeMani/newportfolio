58af5ea96b9319c31254a5ad87d65d382658007b
TojeeMani <tojeemani8@gmail.com>
2025-04-09 19:51:12 +0530

    Portfolio

---
+++ src/components/about/About.jsx
@@ -0,0 +1,60 @@
+import "./about.css";
+import Me from "../../img/tojee.png";
+
+const About = () => {
+
+  const skills = {
+    languages: ["JavaScript", "PHP", "JSX", "HTML", "CSS"],
+    frameworks: ["React.js", "R", "Node.js", "Python", "Django"],
+    libraries: [
+      "Tailwind", "Zustand", "React-router", "Stripe",
+      "Bootstrap", "Material UI", "SCSS", "Unit test"
+    ],
+    cloud: ["AWS-Lambda", "AWS-Cognito"],
+    devTools: ["Visual Studio code", "Git", "Gitlab", "React-dev tools"],
+    backendAndApi: ["REST API", "Authentication (OAuth)", "API Integration"],
+    databases: ["MongoDB", "MySQL"]
+  };
+
+  const skillLabels = {
+    languages: "Languages",
+    frameworks: "Frameworks",
+    libraries: "Libraries",
+    backendAndApi: "Backend and API",
+    cloud: "Cloud",
+    devTools: "Dev Tools",
+    databases: "Databases"
+  };
+
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img src={Me} alt="" className="a-img" />
+        </div>
+      </div>
+      <div className="a-right">
+        <h1 className="a-title">About Me</h1>
+        <p className="a-desc">
+          A highly skilled software professional with a focus on delivering robust React applications. Proven ability to create dynamic and responsive web interfaces, ensuring seamless user experiences. Committed to team collaboration and adaptability, showcasing expertise in JavaScript, HTML, and CSS.
+        </p>
+
+        <div style={{ marginTop: "15px" }}>
+          <h1 className="a-title">Skills</h1>
+          <p className="a-desc">
+            <ul>
+              {Object.keys(skillLabels).map((key) => (
+                <li key={key}>
+                  <strong>{skillLabels[key]}:</strong> {skills[key].join(", ")}
+                </li>
+              ))}
+            </ul>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,101 @@
+import React, { useContext, useRef, useState } from "react";
+import "./contact.css";
+import App from '../../'
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+import emailjs from "@emailjs/browser";
+import { ThemeContext } from "../context";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [done, setDone] = useState(false);
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        formRef.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          setDone(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-left">Contanct Me</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +91 7907850478
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Email} alt="" />
+              tojeemani8@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Address} alt="" />
+              Kochi, Kerala, India
+            </div>
+            <div className="c-info-item">
+              <a href="/melbin's_resume.pdf" download="tojee_resume.pdf" className="resume-download">
+                Download Resume
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <div>
+            <b>What's your story?</b> Get in touch. Always available for
+            freelancing if the right project comes along. me.
+          </div>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Name"
+              name="user_name"
+            />
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Subject"
+              name="user_subject"
+            />
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Email"
+              name="user_email"
+            />
+            <textarea
+              style={{ backgroundColor: darkMode && "#333" }}
+              placeholder="Message"
+              rows="5"
+              col="5"
+              name="message"
+            ></textarea>
+            <button>Submit</button>
+            {done && "Thank You.."}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
